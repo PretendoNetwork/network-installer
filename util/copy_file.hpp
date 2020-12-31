12bcddf7859fee63923d40783247fba5dd0493b7
Ash Logan <ash@heyquark.com>
2020-12-31 21:39:02 +1100

    copy_file: add some safety checks for OSv10 writes

--- util/copy_file.hpp
+++ util/copy_file.hpp
@@ -14,6 +14,11 @@
     //thanks Maschell
     size_t size;
 
+    if (destpath.string().find("1000400a") != std::string::npos) {
+        printf("CRITICAL BUG: tried to write to OSv10!\n");
+        return false;
+    }
+
     int source = open(srcpath.c_str(), O_RDONLY, 0);
     if (source < 0) return false;
     OnLeavingScope _src_c([&] {
@@ -47,6 +52,11 @@
 }
 
 [[maybe_unused]] static bool fast_write_file(const std::filesystem::path& destpath, std::span<uint8_t> data) {
+    if (destpath.string().find("1000400a") != std::string::npos) {
+        printf("CRITICAL BUG: tried to write to OSv10!\n");
+        return false;
+    }
+
     int dest = open(destpath.c_str(), O_CREAT | O_TRUNC | O_WRONLY, 0644);
     if (dest < 0) return false;
     OnLeavingScope _dst_c([&] {
